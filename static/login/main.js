c80f80f8a4027c49576a005b27a270436b0ff7e3
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-07-16 17:19:35 +0200

    maybe save the login

--- static/login/main.js
+++ static/login/main.js
@@ -4,6 +4,7 @@ let input = document.getElementById("input");
 
 window.login = async () => {
   await new Promise((r) => setTimeout(r, 500));
+  cookie.pwd = input.component.value;
   if (await config.checkPwd(input.component.value)) location.pathname += "../";
   input.component.value = "";
 };
